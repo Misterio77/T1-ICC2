2ebe3ffe296eb5c1fcca709ce39ddd72c3c66eb2
LeandroSatoshi <leandro.satoshi@usp.br>
2018-09-19 18:09:08 -0300

    pseudo menu na main

--- src/main.c
+++ src/main.c
@@ -13,14 +13,18 @@ void init(int a[], int n, int step, int range) {
 }
 
 int main(){
-	int *a, size, i;
-	printf("Digite o tamanho do vetor aleatorio: ");
+	int *a, alg, type, size, i;
+	printf("Algoritmos de ordenacao\nEscolha o algoritmo:\n1-Bubblue Sort\n2-Sentinel Bubble Sort\n3-Cocktail sort\n4-Insertion Sort\n5-Selection Sort\n6-Merge Sort\n7-Heap Sort\n8-Quick Sort\n");
+	scanf("%d", &alg);
+	printf("Escolha o tipo de vetor:\n1-Aleatorio\n2-Quase ordenado\n3-Quase inversamente ordenado\n4-Muitos valores repetidos\n");
+	scanf("%d", &type);
+	printf("Digite um K entre 2 e 6 para o tamanho do vetor: 10^K\n");
 	scanf("%d", &size);
 	
 	a = (int*)malloc(sizeof(int)*size);
 	
 	init(a, size, 0, 5 * size);
-	selection_sort(a, size);
+	//selection_sort(a, size);
 	
 	for(i=0; i < size; i++){
 		printf("%d ",a[i]);
